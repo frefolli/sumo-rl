65e40bf1064a66908ba65bc4f0e8bcd07bb881cb
Francesco Refolli <f.refolli@campus.unimib.it>
2025-08-21 13:48:58 +0200

    prova

--- sumo_rl/exploration/plot_epsilon.py
+++ sumo_rl/exploration/plot_epsilon.py
@@ -9,9 +9,10 @@ if __name__ == "__main__":
     prs = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     prs.add_argument("-e", dest="epsilon", type=float, required=True, help="Epsilon\n")
     prs.add_argument("-d", dest="decay", type=float, required=True, help="Epsilon\n")
+    prs.add_argument("-l", dest="length", type=int, required=True, help="Length\n")
     args = prs.parse_args()
 
-    plt.plot([i for i in range(0, 20000, 5)], [args.epsilon * args.decay**i for i in range(0, 4000)])
+    plt.plot([i for i in range(0, args.length, 5)], [args.epsilon * args.decay**i for i in range(0, args.length // 5)])
 
     plt.grid()
     plt.show()
